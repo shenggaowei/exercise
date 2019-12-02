de927526e3e8797729b57bcafd833adecd00a354
weishenggao <>
2019-12-02 21:16:04 +0800

    react-router

--- 编程语言/JS基础/引用类型/正则/basic.js
+++ 编程语言/JS基础/引用类型/正则/basic.js
@@ -1,7 +1,7 @@
 // (x) 捕获组
 {
-    let reg1 = /(foo) (?:bar) \1/
-    let str = 'foo bar foo'
+    let reg1 = /(foo) (?:bar) \1 \2/
+    let str = 'foo bar foo bar'
     let result = reg1.exec(str)
     let reverseStr = str.replace(reg1,'$2 $1')
     console.log(reverseStr,result);
@@ -29,7 +29,7 @@
     console.log(case1[0])
 }
 
-// (?<=y)x 后行断言
+// (?<=y)x 后行断言 y并不是匹配的一部分
 {
     let reg = /(?<=Jack)Sprat/
     let str = 'JackSprat Jack'
@@ -41,4 +41,51 @@
     let name = 'weishenggao'
     let result2 = reg2.exec(name)
     console.log(result2[0])
+}
+
+// /x(?!y)/ 正向否定查找 x后面不跟着y，才会匹配x
+{
+    let reg = /\d+(?!\.)/
+    let str1 = '3.14'
+    let result1 = reg.exec(str1)
+    console.log(result1)
+
+    // \w+会尽可能多的匹配字符
+    // ? 如果只匹配第一个ff 应该怎么做
+    let reg2 = /(?<=wei)\w+(?=dd)/
+    let str2 = 'weishenggaofffffddffffdd'
+    let result2 = reg2.exec(str2)
+    console.log(result2)
+}
+
+// 反向否定查找 /(?<!x)y/ 匹配y，其前面不是x
+{
+    let reg1 = /(?<=-333)\w+/
+    let str = '-33344'
+    let result3 = reg1.exec(str)
+    console.log(result3)
+}
+
+// | 量词 
+{
+
+}
+
+// {} 字符出现次数
+{
+
+}
+
+// [] 字符出现的集合
+{
+
+}
+
+// 边界
+{
+
+}
+
+// 特殊字符
+{
 }
