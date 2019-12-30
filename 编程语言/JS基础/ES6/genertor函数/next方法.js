02054d942ed39b0389f40e324d9a33f3fc3039ae
weishenggao <>
2019-12-30 23:11:32 +0800

    update code

--- 编程语言/JS基础/ES6/genertor函数/next方法.js
+++ 编程语言/JS基础/ES6/genertor函数/next方法.js
@@ -14,3 +14,22 @@
         console.log(data)
     }
 }
+
+{
+    function* foo(x) {
+        var y = 2 * (yield (x + 1));
+        var z = yield (y / 3);
+        return (x + y + z);
+    }
+      
+      var a = foo(5); 
+     console.log(a.next().value) // 5 + 1 = 6,
+     console.log(a.next().value) // NAN 继续上次暂停处执行，yield无返回值，即其值为undefined,相乘得y为nan，第二个 yield后面的值为nan。
+     console.log(a.next().value) // NAN 继续上次暂停处执行，z为undefined, 5 + nan + undefined = nan
+      
+      var b = foo(5);
+      console.log(b.next().value) // 6  x为5，第一个yield后面呢的值为 5+1
+      console.log(b.next(12).value) // 8 第一个yield返回值为12,y = 2*12, 第二个yield后值为 24 / 3
+      console.log(b.next(13).value) // 13 + 24 + 5 = 42 第二个yield返回值为13，即z为13，
+}
+
