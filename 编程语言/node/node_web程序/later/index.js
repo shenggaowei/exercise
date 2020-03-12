52271c2640e2f02ca09801b523d9b552c915d5c3
weishenggao <>
2020-03-12 22:54:36 +0800

    update code

--- 编程语言/node/node_web程序/later/index.js
+++ 编程语言/node/node_web程序/later/index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const read = require('node-readability')
@@ -11,12 +12,17 @@ app.use(bodyParser.json())
 // 支持编码为表单的请求消息体
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// 指定目录，在文件引用时，可以让浏览器获取到。
+// 第一个参数为前缀。第二个参数为指定的静态文件或者文件夹
+app.use('/css',express.static('node_modules/bootstrap/dist/css'))
+app.use('/static', express.static('src'))
+
 const port = process.env.port || 3000
 
 app.get('/articles', (req,res,next) => {
     Article.all((err,articles) => {
         if(err) { throw err }
-        res.render('articles', { articles })
+        res.render('articles.ejs', {articles})
     })
 })
 
