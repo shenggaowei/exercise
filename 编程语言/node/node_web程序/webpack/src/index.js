7ada74313a22fae3be67b1e7244d2ddaa111d12a
weishenggao <>
2020-03-21 01:54:42 +0800

    理解runtime

--- 编程语言/node/node_web程序/webpack/src/index.js
+++ 编程语言/node/node_web程序/webpack/src/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+async function testshenggao(){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve('升高')
+        }, 3000)
+    })
+}
+
+ testshenggao().then(data => {
+     console.log(data)
+ })
+
 function RenderApp(){
     return <div>hello world 升高</div>
 }
