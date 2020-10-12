d83bc3f0a0ab8571a4076f9432fe951af303387b
weishenggao <weishenggao@innobuddy.com>
2020-10-12 10:41:51 +0800

    feat: accumulate 添加思路

--- 编程/JS基础/进阶/技巧/accumulate.js
+++ 编程/JS基础/进阶/技巧/accumulate.js
@@ -1,9 +1,12 @@
-/**
+/** 无限累加函数
  * sum(1,2,3).valueOf() 6
  * sum(2,3)(2).valueof() 7
  * sum(1)(2)(3)(4).valueOf() 10
  * sum(2)(4,1)(2).valueOf() 9
  */
+
+ // 思路： 
+ // 1. sum 函数对参数进行聚集。 2. valueOf 函数对聚集的参数进行累加。
 
 function sum(...args) {
   const f = (...rest) => {
